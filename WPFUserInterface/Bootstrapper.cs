f87cefd984d9ea09aaafd5437c2d63f67b8ab092
conclancy <conclancy@hotmail.com>
2020-12-13 12:55:48 -0500

    Add PasswordBox Helper
    Added a password box helper class to ensure
    we can work with the password box's length
    for validation.  Followed information from:
    https://stackoverflow.com/questions/30631522/caliburn-micro-support-for-passwordbox

--- WPFUserInterface/Bootstrapper.cs
+++ WPFUserInterface/Bootstrapper.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using WPFUserInterface.Helpers;
 using WPFUserInterface.ViewModels;
 
 namespace WPFUserInterface
@@ -16,6 +18,11 @@ namespace WPFUserInterface
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
