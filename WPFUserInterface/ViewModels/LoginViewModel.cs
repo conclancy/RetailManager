4eae9a299fc502edff1b95de64b5c739af85d71e
conclancy <conclancy@hotmail.com>
2020-12-13 13:16:02 -0500

    Add WPF Login View
    Created a WPF Login View that renders in the
    Shell View upon initialization.  Login button is
    unavailable until the username and password
    boxes contain at least 1 character.

---
+++ WPFUserInterface/ViewModels/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUserInterface.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set 
+            { 
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            { 
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn 
+        {
+            get 
+            {
+                bool output = false;
+
+                // ? after propterty test to see if it is null
+                // If _property_ is not null THEN check the length
+                if (UserName?.Length > 0 && Password?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public void LogIn ()
+        {
+            Console.WriteLine();
+        }
+    }
+}
