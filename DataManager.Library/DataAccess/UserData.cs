10883458f3dd494e9d1a25094b7b1c56382927aa
conclancy <conclancy@hotmail.com>
2020-12-13 20:58:59 -0500

    Create UserModel and connection to API/SQL
    Createda user model and wired it up to API and SQL.

---
+++ DataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using DataManager.Library.Internal.DataAccess;
+using DataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserByID(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RetailManagerData");
+
+            return output;
+        }
+    }
+}
